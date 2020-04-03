a4d5fb6ba5a0ce0fe81ef24dfab45131552984d8
Tom Harwood <tharwood26@gmail.com>
2020-04-03 16:20:35 +0100

    views updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @members = %w[thanh dimitri germain damien julien]
+    if params[:member]
+      @members = @members.select { |member| member.starts_with?(params[:member]) }
+    end
   end
 
   def home
